1e8f86c5758d5e3fb37d5576e93a97585e1d32b7
VidBarbaro <76628104+VidBarbaro@users.noreply.github.com>
2023-09-14 14:28:06 +0200

    Add navbar, remove unused components and modify global css file

---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav>
+        <img src="../assets/images/TopicTimerWhiteLogo.png" alt="topic timer logo">
+        <section>
+            <RouterLink class="link" to="/">Subjects</RouterLink>
+            <RouterLink class="link" to="/about">History</RouterLink>
+            <RouterLink class="link" to="/about">User</RouterLink>
+        </section>
+    </nav>
+</template>
+
+<style scoped>
+template {
+    margin: 0;
+    padding: 0;
+}
+
+nav {
+    background-color: #009688;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content:space-between;
+}
+
+img {
+    width: 13rem;
+    margin: 1rem 3rem;
+}
+
+section {
+    display: flex;
+    justify-content: space-evenly;
+    width: 50%;
+    font-size: 1.5rem;
+}
+
+.link {
+    color: #F5F5F5;
+    text-decoration: none;
+}
+</style>
