06212f08f96a654508105b2edd91dadba58f5894
Michel Meulen <michel.meulen@outlook.com>
2023-09-14 09:42:15 +0200

    Created the mobile app

---
+++ TopicTimer - Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TopicTimer___Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
