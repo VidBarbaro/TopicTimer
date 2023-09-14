e3a2ee8809870b50fc9af5f98a53915ce8c443d5
Michel Meulen <michel.meulen@outlook.com>
2023-09-14 09:53:32 +0200

    Created the initial project

---
+++ TopicTimer - Mobile/TopicTimer - Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TopicTimer___Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ TopicTimer - Mobile/TopicTimer - Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TopicTimer___Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
