0600f194d1d42f5c4e1c1774733b727673ba2b3e
TychodeLaat <tycho.dl1209@gmail.com>
2023-09-14 10:31:28 +0200

    Installed bootstrap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,10 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container">
+     <button type="button" class="btn btn-success">Success</button>
+     <button type="button" class="btn btn-info">Info</button>
+     <button type="button" class="btn btn-warning">Warning</button>
+  </div>
   </main>
 </template>
