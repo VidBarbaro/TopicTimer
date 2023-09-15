967ca53055c994dbd9f134605b6488d9081d4576
VidBarbaro <76628104+VidBarbaro@users.noreply.github.com>
2023-09-15 14:49:32 +0200

    Add subjects to a list on subject page for user to view

---
+++ src/components/SubjectListItem.vue
@@ -0,0 +1,70 @@
+<template>
+    <section>
+        <span>
+            <div class="circle" v-bind:style="{ 'background-color': subject.color }">{{ subject.shortName }}</div>
+            <p>{{ subject.name }}</p>
+        </span>
+        <span>
+            <i v-if="subject.editable" class="bi bi-pencil-square"></i>
+            <i v-else class="bi bi-info-circle-fill"></i>
+            <i class="bi bi-trash-fill"></i>
+        </span>
+    </section>
+    <hr>
+</template>
+
+<style scoped>
+section {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px; /* Increased padding */
+}
+
+span {
+    display: flex;
+    align-items: center;
+}
+
+.circle {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px; /* Increased circle size */
+    height: 60px; /* Increased circle size */
+    border-radius: 50%;
+    background-color: #3498db; /* Circle background color */
+    color: #fff; /* Circle text color */
+    font-size: 24px; /* Increased font size */
+    margin-right: 20px; /* Adjusted spacing between circle and paragraph */
+}
+
+p {
+    margin: 0; /* Remove default margin */
+    font-size: 18px; /* Increased font size */
+}
+
+i {
+    font-size: 24px; /* Increased font size for icons */
+    margin-left: 20px; /* Adjusted spacing between icons */
+}
+
+hr {
+    width: 85%;
+    margin: auto;
+}
+</style>
+
+<script>
+export default {
+    props: {
+        subject: {
+            type: Object,
+            // required: true,
+        }
+    },
+    mounted() {
+    console.log(this.subject);
+    }
+}
+</script>
--- src/views/SubjectsView.vue
+++ src/views/SubjectsView.vue
@@ -1,7 +1,32 @@
 <template>
   <div>
-    <h1>This is going to be the color picker</h1>
-    <input type="color" id="color" name="color" value="#e66465" />
-    <label for="color"> Color</label>
+    <section>
+      <h2>Your subjects: </h2>
+      <button>Add+</button>
+    </section>
+    <section>
+      <SubjectListItem :subject="data[0]"/>
+      <SubjectListItem :subject="data[1]"/>
+      <SubjectListItem :subject="data[2]"/>
+      <SubjectListItem :subject="data[3]"/>
+    </section>
   </div>
 </template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+section {
+  width: 80%;
+}
+</style>
+
+<script setup>
+import SubjectListItem from '../components/SubjectListItem.vue';
+import data from '../assets/data.json'
+console.log("Hello")
+console.log(JSON.stringify(data))
+</script>
