d2311bc899fb412b95b6e63e2c3feace88f9237f
TychodeLaat <tycho.dl1209@gmail.com>
2023-09-16 20:21:46 +0200

    Redirect / to /subjects

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      redirect: '/subjects'
+    },
+    {
       path: '/subjects',
       name: 'subjects',
       component: SubjectsView
