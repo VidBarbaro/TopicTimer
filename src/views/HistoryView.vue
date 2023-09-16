0697d1259e502d191d04be471a78597641ef9ce2
TychodeLaat <tycho.dl1209@gmail.com>
2023-09-16 20:18:01 +0200

    Improved history page styling

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,6 +1,7 @@
 <script setup>
-	import Chart from '../components/Chart.vue'
+	import HistoryChart from '../components/HistoryChart.vue'
 	import jsonData from '../assets/data.json'
+	import moment from 'moment'
 
 	import VueDatePicker from '@vuepic/vue-datepicker';
 	import '@vuepic/vue-datepicker/dist/main.css'
@@ -10,38 +11,25 @@
 	<div class="w-75 m-auto d-flex flex-column gap-4">
 		<div class="mt-4 d-flex justify-content-end">
 			<div class="w-25 d-flex gap-4">
-				<select class="topicSelector form-select">
+				<select role='button' class="topicSelector text-white bg-tt-yellow shadow-none border-0 form-select">
 					<option>
 						Dummy item
 					</option>
 				</select>
-				<VueDatePicker v-model="date" format="MM-dd-yyyy" :enableTimePicker="false" :clearable="false" inputClassName="vdp-input" />
+				<VueDatePicker v-model="date" format="MM-dd-yyyy" :enableTimePicker="false" :clearable="false" inputClassName="text-white bg-tt-yellow border-0" />
 			</div>
 		</div>
-		<Chart chartType="bar" :selectedWeekDay="date" :trackedData="jsonData"/>
+		<HistoryChart chartType="bar" :selectedWeekDay="date" :trackedData="jsonData"/>
 	</div>
 </template>
 
 <style>
 .topicSelector {
-	color: white !important;
-	background-color: #FFC107 !important;
-	border: none !important;
 	background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23ffffff' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m2 5 6 6 6-6'/%3e%3c/svg%3e") !important;
 }
-.topicSelector:hover {
-	cursor: pointer !important;
-}
-.topicSelector:focus {
-	box-shadow: none !important;
-}
+
 .dp__input_icon {
 	color: white;
-}
-.vdp-input {
-	color: white;
-	background-color: #FFC107;
-	border: none;
 }
 .dp__today{
 	border-color: #FFC107;
@@ -58,7 +46,7 @@
 export default {
 	data() {
 		return {
-			date: '09-15-2023'
+			date: moment().format('MM-DD-YYYY')
 		}
 	}
 }
