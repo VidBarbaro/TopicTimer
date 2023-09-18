b0b2effff35adfde7f6ca45b100fea1a70795c75
TychodeLaat <tycho.dl1209@gmail.com>
2023-09-18 11:24:22 +0200

    Made chart selectable through select box

--- website/src/views/HistoryView.vue
+++ website/src/views/HistoryView.vue
@@ -11,15 +11,21 @@
 	<div class="w-75 m-auto d-flex flex-column gap-4">
 		<div class="mt-4 d-flex justify-content-end">
 			<div class="w-25 d-flex gap-4">
-				<select role='button' class="topicSelector text-white bg-tt-yellow shadow-none border-0 form-select">
-					<option>
-						Dummy item
+				<select v-model="chartType" role='button' class="topicSelector text-white bg-tt-yellow shadow-none border-0 form-select">
+					<option class="bg-tt-offwhite" disabled>
+						-- Select a chart type --
+					</option>
+					<option class="bg-tt-offwhite text-tt-offblack" v-bind:selected="chartType.toUpperCase() == 'BAR'">
+						Bar
+					</option>
+					<option class="bg-tt-offwhite text-tt-offblack" v-bind:selected="chartType.toUpperCase() == 'DONUT'">
+						Donut
 					</option>
 				</select>
 				<VueDatePicker v-model="date" format="MM-dd-yyyy" :enableTimePicker="false" :clearable="false" inputClassName="text-white bg-tt-yellow border-0" />
 			</div>
 		</div>
-		<HistoryChart chartType="bar" :selectedWeekDay="date" :trackedData="jsonData"/>
+		<HistoryChart :chartType="chartType" :selectedWeekDay="date" :trackedData="jsonData"/>
 	</div>
 </template>
 
@@ -27,7 +33,6 @@
 .topicSelector {
 	background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23ffffff' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m2 5 6 6 6-6'/%3e%3c/svg%3e") !important;
 }
-
 .dp__input_icon {
 	color: white;
 }
@@ -46,8 +51,9 @@
 export default {
 	data() {
 		return {
-			date: moment().format('MM-DD-YYYY')
+			chartType: 'Bar',
+			date: moment().format('MM-DD-YYYY'),
 		}
-	}
+	},
 }
 </script>
