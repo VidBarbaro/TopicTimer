09675051dfeaefa4fe3ef012c6f394b0fbd30cf8
Michel Meulen <michel.meulen@outlook.com>
2023-10-12 13:14:29 +0200

    Moved Router to main.dart

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,25 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:topictimer_flutter_application/pages/home.dart';
-import 'package:topictimer_flutter_application/pages/news.dart';
+import 'package:topictimer_flutter_application/pages/website/home.dart';
+import 'package:topictimer_flutter_application/pages/website/news.dart';
 
 void main() => runApp(const MyApp());
 
-final GoRouter _router = GoRouter(
+final GoRouter websiteRouter = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
-      pageBuilder: (BuildContext context, GoRouterState state) => NoTransitionPage (
-            key: state.pageKey,
-            child: const HomePage(),
-          ),
+      pageBuilder: (BuildContext context, GoRouterState state) =>
+          NoTransitionPage(
+        key: state.pageKey,
+        child: const HomePage(),
+      ),
     ),
     GoRoute(
       path: '/news',
-      pageBuilder: (BuildContext context, GoRouterState state) => NoTransitionPage (
-            key: state.pageKey,
-            child: const NewsPage(),
-          ),
+      pageBuilder: (BuildContext context, GoRouterState state) =>
+          NoTransitionPage(
+        key: state.pageKey,
+        child: const NewsPage(),
+      ),
     ),
   ],
 );
@@ -29,9 +34,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: _router,
-    );
+    if (kIsWeb) {
+      //return website properties
+      return MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routerConfig: websiteRouter,
+      );
+    } else if (Platform.isAndroid) {
+    } else if (Platform.isIOS) {
+    } else if (Platform.isWindows) {
+    } else if (Platform.isLinux) {
+    } else if (Platform.isMacOS) {}
+    return const Placeholder();
   }
 }
