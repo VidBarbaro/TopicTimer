feb990edce4dd28e259c334ac9f81882336a9513
Tycho de Laat <468771@student.fontys.nl>
2023-10-25 12:43:12 +0200

    Added default default

---
+++ flutter/lib/theme/color_scheme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorSchema {
+  static Map<String, dynamic>? get({String theme = 'default'}) {
+    const schemes = {
+      'default': {
+        'primary': Color(0xff009688),
+        'secondary': Color(0xffFFC107),
+        'tertiary': Color(0xff03A9F4),
+        'background': Color(0xffF5F5F5),
+        'text': Color(0xff333333),
+      }
+    };
+
+    return schemes[theme];
+  }
+}
