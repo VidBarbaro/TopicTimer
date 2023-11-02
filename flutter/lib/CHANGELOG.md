9da6311e02336600cec10f51278355f34d6b1d08
Tycho de Laat <468771@student.fontys.nl>
2023-11-02 15:01:36 +0100

    Added news page with items generated from changelog.md

---
+++ flutter/lib/CHANGELOG.md
@@ -0,0 +1,35 @@
+# Changelog
+
+All notable changes to our platform will be documented in this file.
+
+## 1.01
+
+### Added
+
+- **Redesigned Website:** Experience a sleek, modern interface that's as intuitive as it is visually stunning. Our website has undergone a transformation, offering a fresh, contemporary look and improved user experience.
+
+- **Desktop App Makeover:** Elevate your desktop experience with a brand-new look and improved performance. The desktop app now features a redesigned interface, ensuring a cleaner, more organized workspace.
+
+- **Mobile App Transformation:** Our mobile app has been reimagined to provide an unmatched user experience on the go. The new design is optimized for various mobile devices, making it intuitive and responsive.
+
+- **Watch Redesign:** Revolutionize your wrist with a redesigned watch interface that's both stylish and functional. The new watch design enhances both form and function, whether you're tracking fitness goals or managing notifications.
+
+### Changed
+
+- **Improved Watch Responsiveness:** The watch's responsiveness has been significantly enhanced, providing a faster and more intuitive experience for users.
+
+- **Enhanced Time Tracking:** The app's time tracking functionality has been revamped to offer a more efficient and user-friendly experience.
+
+### Removed
+
+- None
+
+### Fixed
+
+- None
+
+## 1.00
+
+### Added
+
+- Initial release of our platform, featuring the original website, desktop app, mobile app, and watch functionality.
---
+++ flutter/lib/components/website/news_page/news_item.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:topictimer_flutter_application/theme/color_themes.dart';
+
+class NewsItem extends StatelessWidget {
+  final Map<String, dynamic> versionInformation;
+
+  const NewsItem({required this.versionInformation, super.key});
+
+  Text formattedVersionInfo() {
+    final added = versionInformation['added'] as List<dynamic>;
+    final changed = versionInformation['changed'] as List<dynamic>;
+    final removed = versionInformation['removed'] as List<dynamic>;
+    final fixed = versionInformation['fixed'] as List<dynamic>;
+
+    final List<TextSpan> textSpans = [];
+
+    if (added.isNotEmpty) {
+      textSpans.add(const TextSpan(
+        text: '\nAdded:\n',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+      for (final item in added) {
+        textSpans.add(
+          TextSpan(text: '- ${item.toString().replaceAll(': ', ':\n')}\n\n'),
+        );
+      }
+    }
+
+    if (changed.isNotEmpty) {
+      textSpans.add(const TextSpan(
+        text: '\nChanged:\n',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+      for (final item in changed) {
+        textSpans.add(
+          TextSpan(text: '- ${item.toString().replaceAll(': ', ':\n')}\n\n'),
+        );
+      }
+    }
+
+    if (removed.isNotEmpty) {
+      textSpans.add(const TextSpan(
+        text: '\nRemoved:\n',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+      for (final item in removed) {
+        textSpans.add(
+          TextSpan(text: '- ${item.toString().replaceAll(': ', ':\n')}\n\n'),
+        );
+      }
+    }
+
+    if (fixed.isNotEmpty) {
+      textSpans.add(const TextSpan(
+        text: '\nFixed:\n',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ));
+      for (final item in fixed) {
+        textSpans.add(
+          TextSpan(text: '- ${item.toString().replaceAll(': ', ':\n')}\n\n'),
+        );
+      }
+    }
+
+    return Text.rich(
+      TextSpan(
+        children: textSpans,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 190, 190, 190).withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 1,
+            offset: const Offset(0.8, 0.8), // changes position of shadow
+          ),
+        ],
+      ),
+      margin: const EdgeInsets.only(top: 30),
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      child: Row(
+        children: [
+          Container(
+            constraints: const BoxConstraints(
+              minHeight: 50,
+              maxHeight: 300,
+            ),
+            width: 150,
+            child: const RotationTransition(
+              turns: AlwaysStoppedAnimation(-45 / 360),
+              child: Image(
+                image: AssetImage('assets/images/TopicTimerFullLogo.png'),
+                height: 50,
+              ),
+            ),
+          ),
+          Container(
+            constraints: const BoxConstraints(
+              minHeight: 50,
+              maxHeight: 300,
+            ),
+            width: 850,
+            child: Scrollbar(
+              thumbVisibility: true,
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: Text.rich(
+                        TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Version ${versionInformation['version']}',
+                            ),
+                          ],
+                          style: TextStyle(
+                            color: ColorThemes.get()?['text'],
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: formattedVersionInfo(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
