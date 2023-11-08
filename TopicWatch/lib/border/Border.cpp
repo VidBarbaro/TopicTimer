08a3af73c032c65dfb1df3cd9f6d6d4ae922110e
Tycho de Laat <468771@student.fontys.nl>
2023-11-08 16:22:26 +0100

    Created a border library

---
+++ TopicWatch/lib/border/Border.cpp
@@ -0,0 +1,63 @@
+#include "Border.h"
+
+Border::Border(TFT_eSPI *tft, int height)
+{
+    _tft = tft;
+    _height = height;
+}
+
+double Border::_getPrecentage(double progress, double goal)
+{
+    return (progress / goal) * 100;
+}
+
+void Border::clear(uint32_t clearColor)
+{
+    _tft->fillRect(0, 0, _screenHorizontal, _height, clearColor);                           // draw top
+    _tft->fillRect((_screenHorizontal - _height), 0, _height, _screenVertical, clearColor); // draw right
+    _tft->fillRect(0, (_screenVertical - _height), _screenHorizontal, _height, clearColor); // draw bottom
+    _tft->fillRect(0, 0, _height, _screenVertical, clearColor);                             // draw left
+}
+
+void Border::set(double precent)
+{
+    Border::set(precent, _defaultColor);
+}
+
+void Border::set(double precent, uint32_t color)
+{
+    double totalBorderWidth = (precent / 100) * 980;
+
+    if (totalBorderWidth <= _screenHorizontal)
+    {
+        _tft->fillRect(0, 0, totalBorderWidth, _height, color);
+    }
+    else if (totalBorderWidth <= (_screenHorizontal + _screenVertical))
+    {
+        _tft->fillRect(0, 0, _screenHorizontal, _height, color); // draw top
+        _tft->fillRect((_screenHorizontal - _height), 0, _height, (totalBorderWidth - _screenHorizontal), color);
+    }
+    else if (totalBorderWidth <= (_screenHorizontal + _screenVertical + _screenHorizontal))
+    {
+        _tft->fillRect(0, 0, _screenHorizontal, _height, color);                           // draw top
+        _tft->fillRect((_screenHorizontal - _height), 0, _height, _screenVertical, color); // draw right
+        _tft->fillRect((_screenHorizontal - (totalBorderWidth - _screenHorizontal - _screenVertical)), (_screenVertical - _height), (totalBorderWidth - _screenHorizontal - _screenVertical), _height, color);
+    }
+    else
+    {
+        _tft->fillRect(0, 0, _screenHorizontal, _height, color);                           // draw top
+        _tft->fillRect((_screenHorizontal - _height), 0, _height, _screenVertical, color); // draw right
+        _tft->fillRect(0, (_screenVertical - _height), _screenHorizontal, _height, color); // draw bottom
+        _tft->fillRect(0, (_screenVertical - (totalBorderWidth - _screenHorizontal - _screenVertical - _screenHorizontal)), _height, (totalBorderWidth - _screenHorizontal - _screenVertical - _screenHorizontal), color);
+    }
+}
+
+void Border::setWithGoal(double progress, double goal)
+{
+    Border::set(_getPrecentage(progress, goal), _defaultColor);
+}
+
+void Border::setWithGoal(double progress, double goal, uint32_t color)
+{
+    Border::set(_getPrecentage(progress, goal), color);
+}
---
+++ TopicWatch/lib/border/Border.h
@@ -0,0 +1,30 @@
+#ifndef BORDER
+#define BORDER
+
+#include "Arduino.h"
+#include "TFT_eSPI.h"
+
+class Border
+{
+private:
+    TFT_eSPI *_tft;
+    int _height = 5;
+    uint32_t _defaultColor = 0xFFFFFF;
+    int _screenCircumference = 980;
+    int _screenHorizontal = 320;
+    int _screenVertical = 170;
+
+    double
+    _getPrecentage(double progress, double goal);
+
+public:
+    Border(TFT_eSPI *tft, int height);
+    ~Border() = default;
+    void clear(uint32_t clearColor);
+    void set(double precent);
+    void set(double precent, uint32_t color);
+    void setWithGoal(double progress, double goal);
+    void setWithGoal(double progress, double goal, uint32_t color);
+};
+
+#endif
