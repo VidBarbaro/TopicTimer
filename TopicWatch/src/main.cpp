9f49efe7464d2aa6c0416997e17fc47bdd066fbe
Tycho de Laat <468771@student.fontys.nl>
2023-11-09 15:39:28 +0100

    Simplified BLE code and implemented get time example

--- TopicWatch/src/main.cpp
+++ TopicWatch/src/main.cpp
@@ -10,75 +10,28 @@ Border border = Border(&tft, 3);
 
 int percentage = 0;
 
-static NimBLEServer *pServer;
+bool connected = false;
+bool sendRequest = false;
+bool waitingForAnswer = false;
+
+static NimBLEServer *pTopicWatchServer;
+NimBLEService *pTopicWatchService;
+NimBLECharacteristic *pTopicWatchCharacteristic;
 
 /**  None of these are required as they will be handled by the library with defaults. **
  **                       Remove as you see fit for your needs                        */
 class ServerCallbacks : public NimBLEServerCallbacks
 {
-    void onConnect(NimBLEServer *pServer)
-    {
-        Serial.println("Client connected");
-        Serial.println("Multi-connect support: start advertising");
-        NimBLEDevice::startAdvertising();
-    };
-    /** Alternative onConnect() method to extract details of the connection.
-     *  See: src/ble_gap.h for the details of the ble_gap_conn_desc struct.
-     */
     void onConnect(NimBLEServer *pServer, ble_gap_conn_desc *desc)
     {
-        tft.drawString("Connected", 50, 40);
-        Serial.print("Client address: ");
-        Serial.println(NimBLEAddress(desc->peer_ota_addr).toString().c_str());
-        /** We can use the connection handle here to ask for different connection parameters.
-         *  Args: connection handle, min connection interval, max connection interval
-         *  latency, supervision timeout.
-         *  Units; Min/Max Intervals: 1.25 millisecond increments.
-         *  Latency: number of intervals allowed to skip.
-         *  Timeout: 10 millisecond increments, try for 5x interval time for best results.
-         */
-        pServer->updateConnParams(desc->conn_handle, 24, 48, 0, 60);
+        connected = true;
+        NimBLEDevice::stopAdvertising();
     };
+
     void onDisconnect(NimBLEServer *pServer)
     {
-        tft.drawString("Disconnected", 50, 40);
-        Serial.println("Client disconnected - start advertising");
+        connected = false;
         NimBLEDevice::startAdvertising();
-    };
-    void onMTUChange(uint16_t MTU, ble_gap_conn_desc *desc)
-    {
-        Serial.printf("MTU updated: %u for connection ID: %u\n", MTU, desc->conn_handle);
-    };
-
-    /********************* Security handled here **********************
-    ****** Note: these are the same return values as defaults ********/
-    uint32_t onPassKeyRequest()
-    {
-        Serial.println("Server Passkey Request");
-        /** This should return a random 6 digit number for security
-         *  or make your own static passkey as done here.
-         */
-        return 123456;
-    };
-
-    bool onConfirmPIN(uint32_t pass_key)
-    {
-        Serial.print("The passkey YES/NO number: ");
-        Serial.println(pass_key);
-        /** Return false if passkeys don't match. */
-        return true;
-    };
-
-    void onAuthenticationComplete(ble_gap_conn_desc *desc)
-    {
-        /** Check that encryption was successful, if not we disconnect the client */
-        if (!desc->sec_state.encrypted)
-        {
-            NimBLEDevice::getServer()->disconnect(desc->conn_handle);
-            Serial.println("Encrypt connection failed - disconnecting client");
-            return;
-        }
-        Serial.println("Starting BLE work!");
     };
 };
 
@@ -92,17 +45,22 @@ class CharacteristicCallbacks : public NimBLECharacteristicCallbacks
         Serial.print(pCharacteristic->getUUID().toString().c_str());
         Serial.print(": onRead(), value: ");
         Serial.println(pCharacteristic->getValue().c_str());
-        tft.drawString(s, 50, 70);
+        tft.drawString(s, 50, 80);
     };
 
     void onWrite(NimBLECharacteristic *pCharacteristic)
     {
-        String s = "Write: ";
-        s += pCharacteristic->getValue().c_str();
-        Serial.print(pCharacteristic->getUUID().toString().c_str());
-        Serial.print(": onWrite(), value: ");
-        Serial.println(pCharacteristic->getValue().c_str());
-        tft.drawString(s, 50, 70);
+        if (waitingForAnswer)
+        {
+            border.set(100, TFT_YELLOW);
+
+            String s = pCharacteristic->getValue().c_str();
+            tft.setTextSize(5);
+            tft.setTextDatum(MC_DATUM);
+            tft.drawString(s, tft.width() / 2, tft.height() / 2);
+
+            waitingForAnswer = false;
+        }
     };
     /** Called before notification or indication is sent,
      *  the value can be changed here before sending if desired.
@@ -111,72 +69,11 @@ class CharacteristicCallbacks : public NimBLECharacteristicCallbacks
     {
         Serial.println("Sending notification to clients");
     };
-
-    /** The status returned in status is defined in NimBLECharacteristic.h.
-     *  The value returned in code is the NimBLE host return code.
-     */
-    void onStatus(NimBLECharacteristic *pCharacteristic, Status status, int code)
-    {
-        String str = ("Notification/Indication status code: ");
-        str += status;
-        str += ", return code: ";
-        str += code;
-        str += ", ";
-        str += NimBLEUtils::returnCodeToString(code);
-        Serial.println(str);
-    };
-
-    void onSubscribe(NimBLECharacteristic *pCharacteristic, ble_gap_conn_desc *desc, uint16_t subValue)
-    {
-        String str = "Client ID: ";
-        str += desc->conn_handle;
-        str += " Address: ";
-        str += std::string(NimBLEAddress(desc->peer_ota_addr)).c_str();
-        if (subValue == 0)
-        {
-            str += " Unsubscribed to ";
-        }
-        else if (subValue == 1)
-        {
-            str += " Subscribed to notfications for ";
-        }
-        else if (subValue == 2)
-        {
-            str += " Subscribed to indications for ";
-        }
-        else if (subValue == 3)
-        {
-            str += " Subscribed to notifications and indications for ";
-        }
-        str += std::string(pCharacteristic->getUUID()).c_str();
-
-        Serial.println(str);
-    };
 };
-
-/** Handler class for descriptor actions */
-class DescriptorCallbacks : public NimBLEDescriptorCallbacks
-{
-    void onWrite(NimBLEDescriptor *pDescriptor)
-    {
-        std::string dscVal = pDescriptor->getValue();
-        Serial.print("Descriptor witten value:");
-        Serial.println(dscVal.c_str());
-    };
-
-    void onRead(NimBLEDescriptor *pDescriptor)
-    {
-        Serial.print(pDescriptor->getUUID().toString().c_str());
-        Serial.println(" Descriptor read");
-    };
-};
-
-/** Define callback instances globally to use for multiple Charateristics \ Descriptors */
-static DescriptorCallbacks dscCallbacks;
-static CharacteristicCallbacks chrCallbacks;
 
 void setup()
 {
+    Serial.begin(9600);
     /*
      * Turn on and initialize the screen
      */
@@ -194,54 +91,29 @@ void setup()
 
     /** Optional: set the transmit power, default is 3db */
 #ifdef ESP_PLATFORM
-    NimBLEDevice::setPower(ESP_PWR_LVL_P9); /** +9db */
+    NimBLEDevice::setPower(ESP_PWR_LVL_P21); /** +9db */
 #else
     NimBLEDevice::setPower(9); /** +9db */
 #endif
+    NimBLEDevice::setSecurityAuth(false, false, false);
 
-    /** Set the IO capabilities of the device, each option will trigger a different pairing method.
-     *  BLE_HS_IO_DISPLAY_ONLY    - Passkey pairing
-     *  BLE_HS_IO_DISPLAY_YESNO   - Numeric comparison pairing
-     *  BLE_HS_IO_NO_INPUT_OUTPUT - DEFAULT setting - just works pairing
-     */
-    // NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_ONLY); // use passkey
-    // NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_YESNO); //use numeric comparison
+    pTopicWatchServer = NimBLEDevice::createServer();
+    pTopicWatchServer->setCallbacks(new ServerCallbacks());
 
-    /** 2 different ways to set security - both calls achieve the same result.
-     *  no bonding, no man in the middle protection, secure connections.
-     *
-     *  These are the default values, only shown here for demonstration.
-     */
-    // NimBLEDevice::setSecurityAuth(false, false, true);
-    NimBLEDevice::setSecurityAuth(/*BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
-
-    pServer = NimBLEDevice::createServer();
-    pServer->setCallbacks(new ServerCallbacks());
-
-    NimBLEService *pDeadService = pServer->createService(BLE_UUID_SERVICE);
-    NimBLECharacteristic *pBeefCharacteristic = pDeadService->createCharacteristic(
+    pTopicWatchService = pTopicWatchServer->createService(BLE_UUID_SERVICE);
+    pTopicWatchCharacteristic = pTopicWatchService->createCharacteristic(
         BLE_UUID_CHARACTERISTIC,
         NIMBLE_PROPERTY::READ |
-            NIMBLE_PROPERTY::WRITE);
-
-    pBeefCharacteristic->setValue("Burger");
-    pBeefCharacteristic->setCallbacks(&chrCallbacks);
-
-    /** 2904 descriptors are a special case, when createDescriptor is called with
-     *  0x2904 a NimBLE2904 class is created with the correct properties and sizes.
-     *  However we must cast the returned reference to the correct type as the method
-     *  only returns a pointer to the base NimBLEDescriptor class.
-     */
-    NimBLE2904 *pBeef2904 = (NimBLE2904 *)pBeefCharacteristic->createDescriptor("2904");
-    pBeef2904->setFormat(NimBLE2904::FORMAT_UTF8);
-    pBeef2904->setCallbacks(&dscCallbacks);
+            NIMBLE_PROPERTY::WRITE |
+            NIMBLE_PROPERTY::NOTIFY);
+    pTopicWatchCharacteristic->setCallbacks(new CharacteristicCallbacks());
 
     /** Start the services when finished creating all Characteristics and Descriptors */
-    pDeadService->start();
+    pTopicWatchService->start();
 
     NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
     /** Add the services to the advertisment data **/
-    pAdvertising->addServiceUUID(pDeadService->getUUID());
+    pAdvertising->addServiceUUID(pTopicWatchService->getUUID());
     /** If your device is battery powered you may consider setting scan response
      *  to false as it will extend battery life at the expense of less data sent.
      */
@@ -253,21 +125,18 @@ void setup()
      */
     tft.setTextSize(1);
     tft.drawString(BLE_DEVICE_NAME, 50, 10);
+    border.set(100, TFT_RED);
 }
 
 void loop()
 {
-    if (percentage > 100)
+    if (connected && !sendRequest)
     {
-        delay(1000);
-        border.clear(TFT_BLACK);
-        percentage = 0;
-        delay(5000);
-    }
-    else
-    {
-        border.set(percentage, (uint32_t)0xFFFF00);
-        percentage++;
-        delay(50);
+        border.set(100, 0x009688);
+        delay(2000);
+        pTopicWatchCharacteristic->setValue("Time?");
+        pTopicWatchCharacteristic->notify();
+        sendRequest = true;
+        waitingForAnswer = true;
     }
 }
