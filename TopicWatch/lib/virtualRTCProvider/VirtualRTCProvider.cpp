52f78a41a4d4dae774c9dc3c80305b11db1a4432
Tycho de Laat <468771@student.fontys.nl>
2023-11-13 14:22:54 +0100

    Added virtual RTC provider

---
+++ TopicWatch/lib/virtualRTCProvider/VirtualRTCProvider.cpp
@@ -0,0 +1,77 @@
+#include "VirtualRTCProvider.h"
+
+hw_timer_t *VirtualRTCProvider::_timer;
+DateTime VirtualRTCProvider::_dateTime;
+
+VirtualRTCProvider::VirtualRTCProvider()
+{
+    _timer = timerBegin(0, 80, true);
+
+    timerAttachInterrupt(_timer, &VirtualRTCProvider::onTimer, true);
+    timerAlarmWrite(_timer, _sleepTime, true);
+    timerAlarmEnable(_timer);
+
+    setTime(0, 0, 0, 2023, 11, 13);
+}
+
+void IRAM_ATTR VirtualRTCProvider::onTimer()
+{
+    increaseTime();
+}
+
+void VirtualRTCProvider::increaseTime()
+{
+    _dateTime.seconds++;
+
+    if (_dateTime.seconds == 60)
+    {
+        _dateTime.seconds = 0;
+        _dateTime.minutes++;
+
+        if (_dateTime.minutes == 60)
+        {
+            _dateTime.minutes = 0;
+            _dateTime.hours++;
+
+            if (_dateTime.hours == 24)
+            {
+                _dateTime.hours = 0;
+                increaseDate();
+            }
+        }
+    }
+}
+
+void VirtualRTCProvider::increaseDate()
+{
+    static const int _numberOfDaysInTheMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    _dateTime.day++;
+
+    if (_dateTime.day == _numberOfDaysInTheMonth[_dateTime.month - 1] + 1)
+    {
+        _dateTime.day = 1;
+        _dateTime.month++;
+
+        if (_dateTime.month == 13)
+        {
+            _dateTime.month = 1;
+            _dateTime.year++;
+        }
+    }
+}
+
+void VirtualRTCProvider::setTime(int hours, int minutes, int seconds, int year, int month, int day)
+{
+    _dateTime.hours = hours;
+    _dateTime.minutes = minutes;
+    _dateTime.seconds = seconds;
+    _dateTime.year = year;
+    _dateTime.month = month;
+    _dateTime.day = day;
+}
+
+DateTime *VirtualRTCProvider::getTime()
+{
+    return &_dateTime;
+}
---
+++ TopicWatch/lib/virtualRTCProvider/VirtualRTCProvider.h
@@ -0,0 +1,34 @@
+#ifndef VIRTUAL_RTC_PROVIDER
+#define VIRTUAL_RTC_PROVIDER
+
+#include "Arduino.h"
+
+struct DateTime
+{
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+    int year = 0;
+    int month = 0;
+    int day = 0;
+};
+
+class VirtualRTCProvider
+{
+private:
+    static hw_timer_t *_timer;
+    const uint64_t _sleepTime = 1 * 1000000; // 1 second(s)
+    static DateTime _dateTime;
+
+    static void IRAM_ATTR onTimer();
+    static void increaseTime();
+    static void increaseDate();
+
+public:
+    VirtualRTCProvider();
+    ~VirtualRTCProvider() = default;
+    void setTime(int hours, int minutes, int seconds, int year, int month, int day);
+    DateTime *getTime();
+};
+
+#endif
