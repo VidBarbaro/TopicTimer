4826c747920ce9972bb06feb8061fdfd903eb4f1
Tycho de Laat <468771@student.fontys.nl>
2023-11-16 11:45:01 +0100

    Added touch controll

---
+++ TopicWatch/lib/touchSubscriber/TouchSubscriber.cpp
@@ -0,0 +1,63 @@
+#include "TouchSubscriber.h"
+#include "CST816Touch.h"
+
+namespace MDO
+{
+
+    // the pointer is the 'this' from the CST816Touch source
+    /*virtual*/ void TouchSubscriber::gestureNotification(CST816Touch *pTouch, int iGestureId, bool bReleasedScreen)
+    {
+        Serial.print("Gesture");
+        if (bReleasedScreen)
+        {
+            Serial.print(" release");
+        }
+        Serial.print(" detected: ");
+        Serial.print(CST816Touch::gestureIdToString(iGestureId));
+
+        if ((pTouch != 0) && ((iGestureId == (int)CST816Touch::gesture_t::GESTURE_LONG_PRESS) ||
+                              (iGestureId == (int)CST816Touch::gesture_t::GESTURE_DOUBLE_CLICK)))
+        {
+            int x = 0;
+            int y = 0;
+            CST816Touch::gesture_t eGesture;
+
+            pTouch->getLastGesture(eGesture, x, y);
+
+            Serial.print(" at: (");
+            Serial.print(x);
+            Serial.print(",");
+            Serial.print(y);
+            Serial.print(")");
+        }
+
+        Serial.println();
+        _gestureCallback(pTouch, CST816Touch::gestureIdToString(iGestureId), bReleasedScreen);
+    }
+
+    /*virtual*/ void TouchSubscriber::setGestureCallback(void (*func)(CST816Touch *pTouch, String iGestureId, bool bReleasedScreen))
+    {
+        _gestureCallback = func;
+    }
+
+    // the pointer is the 'this' from the CST816Touch source
+    /*virtual*/ void TouchSubscriber::touchNotification(CST816Touch *pTouch, int x, int y, bool bReleasedScreen)
+    {
+        Serial.print("Touch");
+        _touchCallback(pTouch, x, y, bReleasedScreen);
+    }
+
+    /*virtual*/ void TouchSubscriber::setTouchCallback(void (*func)(CST816Touch *pTouch, int x, int y, bool bReleasedScreen))
+    {
+        _touchCallback = func;
+    }
+
+    TouchSubscriber::TouchSubscriber()
+    {
+    }
+
+    TouchSubscriber::~TouchSubscriber()
+    {
+    }
+
+} // namespace end
---
+++ TopicWatch/lib/touchSubscriber/TouchSubscriber.h
@@ -0,0 +1,32 @@
+#ifndef TouchSubscriber_H
+#define TouchSubscriber_H
+
+#include "Arduino.h"
+#include "TouchSubscriberInterface.h"
+
+namespace MDO
+{
+    typedef void (*GestureCallback)(CST816Touch *pTouch, String iGestureId, bool bReleasedScreen);
+    typedef void (*TouchCallback)(CST816Touch *pTouch, int x, int y, bool bReleasedScreen);
+    class TouchSubscriber : public TouchSubscriberInterface
+    {
+    private:
+        GestureCallback _gestureCallback;
+        TouchCallback _touchCallback;
+
+    public:
+        // the pointer is the 'this' from the CST816Touch source
+        virtual void
+        gestureNotification(CST816Touch *pTouch, int iGestureId, bool bReleasedScreen);
+        virtual void setGestureCallback(void (*func)(CST816Touch *pTouch, String iGestureId, bool bReleasedScreen));
+
+        // the pointer is the 'this' from the CST816Touch source
+        virtual void touchNotification(CST816Touch *pTouch, int x, int y, bool bReleasedScreen);
+        virtual void setTouchCallback(void (*func)(CST816Touch *pTouch, int x, int y, bool bReleasedScreen));
+        TouchSubscriber();
+        virtual ~TouchSubscriber();
+    };
+
+} // namespace end
+
+#endif
