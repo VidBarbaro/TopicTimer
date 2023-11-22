687cbac0551cee4f15cdc0996ad190814c4c907a
VidBarbaro <76628104+VidBarbaro@users.noreply.github.com>
2023-11-22 19:35:54 +0100

    Updated the color setup on the topbar and navbar (not complete yet)

--- flutter/lib/components/mobile/topbar.dart
+++ flutter/lib/components/mobile/topbar.dart
@@ -3,22 +3,26 @@ import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 import 'package:topictimer_flutter_application/components/mobile/providers/timer_info_provider.dart';
 import 'package:topictimer_flutter_application/components/mobile/providers/topbar_content_provider.dart';
+import 'package:topictimer_flutter_application/theme/color_provider.dart';
+import 'package:topictimer_flutter_application/theme/custom_color.dart';
 
 class TopBar extends StatelessWidget {
   TopBar({super.key});
   void addTopic() {}
   final ButtonStyle topBarButtonStyle = ElevatedButton.styleFrom(
-      backgroundColor: Colors.white,
+      backgroundColor: ColorProvider.get(CustomColor.background),
       elevation: 5,
-      disabledBackgroundColor: Colors.white70);
-
+      disabledBackgroundColor: ColorProvider.get(CustomColor.primary));
+// THINGS TO DISCUSS WITH MICHEL
+// the changing of the pages, why dont the two left and right pointer buttons dissapear
+// why doesnt the plus button appear on the topics page
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       Container(
           width: 100.w,
           height: 12.h,
-          color: Colors.blue,
+          color: ColorProvider.get(CustomColor.primary),
           child: Wrap(
               alignment: WrapAlignment.spaceAround,
               runAlignment: WrapAlignment.center,
@@ -34,9 +38,9 @@ class TopBar extends StatelessWidget {
                               .read<TopBarConentProvider>()
                               .switchTopicLeft(),
                       style: topBarButtonStyle,
-                      child: const Icon(
+                      child: Icon(
                         Icons.arrow_back,
-                        color: Colors.black,
+                        color: ColorProvider.get(CustomColor.primary),
                       ),
                     );
                   }),
@@ -45,7 +49,11 @@ class TopBar extends StatelessWidget {
                   //Empty button for spacing the topbar
                   TextButton(
                     onPressed: () {},
-                    child: const Icon(Icons.abc, color: Colors.blue),
+                    child: Icon(
+                      Icons.abc,
+                      // NEED TO TALK TO MICHEL ABOUT THIS
+                      color: ColorProvider.get(CustomColor.primary)
+                      ),
                   ),
                 Container(
                   width: 35.w,
@@ -82,9 +90,9 @@ class TopBar extends StatelessWidget {
                                 .read<TopBarConentProvider>()
                                 .switchTopicRight(),
                         style: topBarButtonStyle,
-                        child: const Icon(
+                        child: Icon(
                           Icons.arrow_forward,
-                          color: Colors.black,
+                          color: ColorProvider.get(CustomColor.primary),
                         ));
                   }),
                 if (context.read<TopBarConentProvider>().getSelectedPage() ==
@@ -92,21 +100,24 @@ class TopBar extends StatelessWidget {
                   ElevatedButton(
                       onPressed: () => addTopic(),
                       style: topBarButtonStyle,
-                      child: const Icon(
+                      child: Icon(
                         Icons.add,
-                        color: Colors.black,
+                        // NEED TO TALK TO MICHEL ABOUT THIS
+                        color: ColorProvider.get(CustomColor.primary),
                       )),
               ])),
       Container(
         width: 100.w,
         height: 5.h,
-        color: Colors.lightBlue,
+        color: ColorProvider.get(CustomColor.tertiary),
         alignment: Alignment.center,
-        child: const Wrap(alignment: WrapAlignment.center, children: <Widget>[
-          Icon(Icons.watch, color: Colors.white),
+        child: Wrap(alignment: WrapAlignment.center, children: <Widget>[
+          Icon(
+            Icons.watch, 
+            color: ColorProvider.get(CustomColor.background)),
           Text(
             'Disconnected',
-            style: TextStyle(color: Colors.white, fontSize: 20),
+            style: TextStyle(color: ColorProvider.get(CustomColor.background), fontSize: 20),
           )
         ]),
       )
