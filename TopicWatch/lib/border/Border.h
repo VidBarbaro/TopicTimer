5955c389b7302f7cdc7500c26141146f06bb3006
Tycho de Laat <468771@student.fontys.nl>
2023-11-22 17:30:59 +0100

    Changed border size to a global watch setting

--- TopicWatch/lib/border/Border.h
+++ TopicWatch/lib/border/Border.h
@@ -3,12 +3,12 @@
 
 #include "Arduino.h"
 #include "TFT_eSPI.h"
+#include "WatchSettings.h"
 
 class Border
 {
 private:
     TFT_eSPI *_tft;
-    int _height = 5;
     uint32_t _defaultColor = 0xFFFFFF;
     int _screenCircumference = 980;
     int _screenHorizontal = 320;
@@ -19,7 +19,7 @@ private:
 public:
     Border() = default;
     ~Border() = default;
-    void init(TFT_eSPI *tft, int height);
+    void init(TFT_eSPI *tft);
     void clear(uint32_t clearColor);
     void set(double precent);
     void set(double precent, uint32_t color);
--- TopicWatch/lib/watchSettings/WatchSettings.h
+++ TopicWatch/lib/watchSettings/WatchSettings.h
@@ -9,6 +9,8 @@ private:
 public:
     static const int maxAmountOfViews = 100;
     static const int minimalTrackingMinutes = 5;
+    static const int borderSize = 4;
+    static const int marginFromBorder = 8;
     static const uint32_t topicTimer_GREEN = 0x009688;
     static const uint32_t topicTimer_ORANGE = 0xFFC107;
     static const uint32_t topicTimer_BLUE = 0x03A9F4;
