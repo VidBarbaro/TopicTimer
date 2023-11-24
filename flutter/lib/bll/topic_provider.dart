1f934b23f7d9fd74ff912878f8c33690df9e87e0
Michel Meulen <michel.meulen@outlook.com>
2023-11-24 13:00:04 +0100

    added startup bluetooth adapter at beginning

--- flutter/lib/bll/topic_provider.dart
+++ flutter/lib/bll/topic_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:topictimer_flutter_application/components/mobile/models/topic_model.dart';
 
 class TopicProvider {
