e7c66d5237ef2ff9b38e909036d685acca240a35
Tycho de Laat <468771@student.fontys.nl>
2023-12-06 16:25:15 +0100

    Color

--- flutter/lib/pages/mobile/home.dart
+++ flutter/lib/pages/mobile/home.dart
@@ -42,54 +42,58 @@ class HomePageMobileState extends State<HomePageMobile> {
       PersistentBottomNavBarItem(
           title: 'Topics',
           textStyle: TextStyle(
-            color: ColorProvider.get(CustomColor.tertiary),
+            color: ColorProvider.get(CustomColor.background),
           ),
           activeColorPrimary:
               ColorProvider.get(CustomColor.secondary) ?? Colors.white,
           icon: Icon(Icons.subject,
               color: ColorProvider.get(CustomColor.secondary)),
           inactiveIcon: Icon(Icons.subject,
-              color: ColorProvider.get(CustomColor.tertiary))),
+              color: ColorProvider.get(CustomColor.background))),
       PersistentBottomNavBarItem(
           title: 'Planning',
-          textStyle: TextStyle(color: ColorProvider.get(CustomColor.tertiary)),
+          textStyle:
+              TextStyle(color: ColorProvider.get(CustomColor.background)),
           activeColorPrimary:
               ColorProvider.get(CustomColor.secondary) ?? Colors.white,
           icon: Icon(Icons.calendar_today,
               color: ColorProvider.get(CustomColor.secondary)),
           inactiveIcon: Icon(Icons.calendar_today,
-              color: ColorProvider.get(CustomColor.tertiary))),
+              color: ColorProvider.get(CustomColor.background))),
       // HAVE TO ASK MICHEL, is the button active the whole time because it's weird with changing colors and stays on the color as it is active the whole time
       // this it has to do something with intial index but not sure
       PersistentBottomNavBarItem(
           title: 'Timer',
-          textStyle: TextStyle(color: ColorProvider.get(CustomColor.tertiary)),
+          textStyle:
+              TextStyle(color: ColorProvider.get(CustomColor.background)),
           activeColorPrimary:
               ColorProvider.get(CustomColor.secondary) ?? Colors.white,
-          icon:
-              Icon(Icons.timer, color: ColorProvider.get(CustomColor.tertiary)),
+          icon: Icon(Icons.timer,
+              color: ColorProvider.get(CustomColor.background)),
           inactiveIcon: Icon(Icons.timer,
               color: ColorProvider.get(CustomColor.background))),
       PersistentBottomNavBarItem(
           title: 'Personal',
-          textStyle: TextStyle(color: ColorProvider.get(CustomColor.tertiary)),
+          textStyle:
+              TextStyle(color: ColorProvider.get(CustomColor.background)),
           activeColorPrimary:
               ColorProvider.get(CustomColor.secondary) ?? Colors.white,
           icon: Icon(Icons.person,
               color: ColorProvider.get(CustomColor.secondary)),
           inactiveIcon: Icon(
             Icons.person,
-            color: ColorProvider.get(CustomColor.tertiary),
+            color: ColorProvider.get(CustomColor.background),
           )),
       PersistentBottomNavBarItem(
           title: 'Settings',
-          textStyle: TextStyle(color: ColorProvider.get(CustomColor.tertiary)),
+          textStyle:
+              TextStyle(color: ColorProvider.get(CustomColor.background)),
           activeColorPrimary:
               ColorProvider.get(CustomColor.secondary) ?? Colors.white,
           icon: Icon(Icons.settings,
               color: ColorProvider.get(CustomColor.secondary)),
           inactiveIcon: Icon(Icons.settings,
-              color: ColorProvider.get(CustomColor.tertiary)))
+              color: ColorProvider.get(CustomColor.background)))
     ];
   }
 
