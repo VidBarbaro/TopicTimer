81685fadb37f46bf2334e80d48a65b7bbbdf9e84
Tycho de Laat <468771@student.fontys.nl>
2024-01-15 23:12:30 +0100

    Added new patterns to the factory

--- TopicWatch/lib/patterns/PatternFactory.cpp
+++ TopicWatch/lib/patterns/PatternFactory.cpp
@@ -8,6 +8,12 @@ VibrationFeedbackPattern *PatternFactory::createVibrationPattern(int patternValu
         return new VibrationPattern_1();
     case 2:
         return new VibrationPattern_2();
+    case 3:
+        return new VibrationPattern_3();
+    case 4:
+        return new VibrationPattern_4();
+    case 5:
+        return new VibrationPattern_5();
     default:
         return nullptr;
     }
@@ -19,8 +25,14 @@ SoundFeedbackPattern *PatternFactory::createSoundPattern(int patternValue)
     {
     case 1:
         return new SoundPattern_1();
-    // case 2:
-    //     return new SoundPattern_2();
+    case 2:
+        return new SoundPattern_2();
+    case 3:
+        return new SoundPattern_3();
+    case 4:
+        return new SoundPattern_4();
+    case 5:
+        return new SoundPattern_5();
     default:
         return nullptr;
     }
--- TopicWatch/lib/patterns/PatternFactory.h
+++ TopicWatch/lib/patterns/PatternFactory.h
@@ -6,8 +6,15 @@
 
 #include "VibrationPattern_1.h"
 #include "VibrationPattern_2.h"
+#include "VibrationPattern_3.h"
+#include "VibrationPattern_4.h"
+#include "VibrationPattern_5.h"
 
 #include "SoundPattern_1.h"
+#include "SoundPattern_2.h"
+#include "SoundPattern_3.h"
+#include "SoundPattern_4.h"
+#include "SoundPattern_5.h"
 
 class PatternFactory
 {
